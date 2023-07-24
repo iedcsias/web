f3af47bbf54383a347fbe1c4cf6ea56a52eeecd6
assimraz <aseemraz@gmail.com>
2023-07-24 21:32:30 +0530

    update gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,7 @@
   // ======= Sticky
   window.onscroll = function () {
     const ud_header = document.querySelector(".ud-header");
-    const sticky = ud_header.offsetTop;
     const logo = document.querySelector(".navbar-brand img");
-
-    if (window.pageYOffset > sticky) {
-      ud_header.classList.add("sticky");
-    } else {
-      ud_header.classList.remove("sticky");
-    }
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
@@ -91,3 +84,92 @@
     scrollTo(document.documentElement);
   };
 })();
+
+$(function () {
+  var swiper = new Swiper(".carousel-gallery .swiper-container", {
+    effect: "slide",
+    speed: 900,
+    slidesPerView: 5,
+    spaceBetween: 20,
+    simulateTouch: true,
+    autoplay: {
+      delay: 5000,
+      stopOnLastSlide: false,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".carousel-gallery .swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      // when window width is <= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 5,
+      },
+      // when window width is <= 480px
+      425: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      // when window width is <= 640px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  }); /*http://idangero.us/swiper/api/*/
+});
+const galleryItems = document.getElementById("gallery-swiper");
+const galleryImgs = [
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+  "12.jpg",
+  "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+  "17.jpg",
+  "18.jpg",
+  "19.jpg",
+  "20.jpg",
+  "21.jpg",
+  "22.jpg",
+  "23.jpg",
+  "24.jpg",
+  "25.jpg",
+  "26.jpg",
+  "27.jpg",
+  "28.jpg",
+  "29.jpg",
+  "30.jpg",
+  "31.jpg",
+  "32.jpg",
+  "33.jpg",
+  "34.jpg",
+];
+const htmlString = galleryImgs
+  .map((item, index) => {
+    return `
+<div class="swiper-slide">
+        <a href="./assets/images/gallery/${item}" data-fancybox="gallery">
+            <div class="image" style="background-image: url(./assets/images/gallery/${item})">
+                <div class="overlay">
+                    <em class="mdi mdi-magnify-plus"></em>
+                </div>
+            </div>
+        </a>
+    </div>
+`;
+  })
+  .join("");
+galleryItems.innerHTML = htmlString;
